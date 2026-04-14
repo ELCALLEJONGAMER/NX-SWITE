250237e28c0110be5c123f62b4d83836883a3de6
ELCALLEJONGAMER <anetto.official@gmail.com>
2026-04-14 01:42:14 -0600

    Cambios de VS

--- NX-Suite/obj/Debug/net8.0-windows/NX-Suite.AssemblyInfo.cs
+++ NX-Suite/obj/Debug/net8.0-windows/NX-Suite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NX-Suite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99017ec22e1af851fee729c4083a556a23bad8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbbbfc10bf5344b4d9aae6bfe4753197ed43f8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NX-Suite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NX-Suite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
