b4a233359b904cba62d0f6ce42f43ac2aaa45a12
ELCALLEJONGAMER <anetto.official@gmail.com>
2026-04-19 22:17:46 -0600

    Merge con mejoras de sonido y splasn

--- NX-Suite/obj/Debug/net8.0-windows/App.g.i.cs
+++ NX-Suite/obj/Debug/net8.0-windows/App.g.i.cs
@@ -58,7 +58,7 @@ namespace NX_Suite {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/NX-Suite;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NX-Suite;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
