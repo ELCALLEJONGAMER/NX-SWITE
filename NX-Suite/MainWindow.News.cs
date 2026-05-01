6a646ad9deaa8fcbe22b4b16869da1abbe31dc0c
ELCALLEJONGAMER <anetto.official@gmail.com>
2026-05-01 03:44:56 -0600

    Se añade hover a las noticias con sonido

--- NX-Suite/MainWindow.News.cs
+++ NX-Suite/MainWindow.News.cs
@@ -1,5 +1,6 @@
 using NX_Suite.Models;
 using NX_Suite.Core;
+using NX_Suite.Models;
 using NX_Suite.UI;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ namespace NX_Suite
     /// </summary>
     public partial class MainWindow
     {
+        private NewsItem? _newsHoverActual;
+
         private void MostrarVistaInicio()
         {
             bool estabaCargando = _cargandoCatalogoInicial;
@@ -53,6 +56,10 @@ namespace NX_Suite
 
             ListaNews.ItemsSource = news;
             TxtSinNews.Visibility = news.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+            ListaNews.PreviewMouseMove -= ListaNews_PreviewMouseMove;
+            ListaNews.PreviewMouseMove += ListaNews_PreviewMouseMove;
+            ListaNews.MouseLeave -= ListaNews_MouseLeave;
+            ListaNews.MouseLeave += ListaNews_MouseLeave;
             Dispatcher.InvokeAsync(ConectarHoverNews);
         }
 
@@ -90,6 +97,21 @@ namespace NX_Suite
         {
             if (_cargandoCatalogoInicial) return;
             Servicios.Sonidos.Reproducir(EventoSonido.Hover);
+        }
+
+        private void ListaNews_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_cargandoCatalogoInicial) return;
+            if ((e.OriginalSource as FrameworkElement)?.DataContext is not NewsItem news) return;
+            if (ReferenceEquals(_newsHoverActual, news)) return;
+
+            _newsHoverActual = news;
+            Servicios.Sonidos.Reproducir(EventoSonido.Hover);
+        }
+
+        private void ListaNews_MouseLeave(object sender, MouseEventArgs e)
+        {
+            _newsHoverActual = null;
         }
 
         private static void AbrirLinkNews(string link)
